956bc6d78cf01a177a7acc962de7dff35545d39b
Davies Ningthouja <meiteidavies@gmail.com>
2026-09-09 14:57:28 +0530

    feat: added category page with table and its data from /api/category

---
+++ src/app/(dashboard)/categories/page.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { CategoryService } from "@/lib/api";
+import {CategoryTable} from "@/components/categories/CategoryTable";
+
+export default async function CategoriesPage(){
+    const response = await CategoryService.getAll();
+    return (
+    <div>
+      <div className="mb-6 flex items-center justify-between">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Categories
+          </h1>
+
+          <p className="mt-1 text-sm text-muted-foreground">
+            Manage your cafe branches.
+          </p>
+        </div>
+
+        <Link
+          href="/branches/new"
+          className="rounded-app bg-primary px-4 py-2 text-sm font-medium text-primary-foreground hover:opacity-90"
+        >
+          + Create Categories
+        </Link>
+      </div>
+        <CategoryTable categories={response.data} /> 
+    </div>
+  );
+}
---
+++ src/components/categories/CategoryTable.tsx
@@ -0,0 +1,69 @@
+import Link from "next/link";
+
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableHead,
+  TableCell,
+} from "@/components/ui/Table/Table";
+
+import type { Category } from "@/types/category";
+
+interface CategoryTableProps {
+  categories: Category[];
+}
+
+export function CategoryTable({
+  categories,
+}: CategoryTableProps) {
+  return (
+    <div className="overflow-hidden rounded-app border bg-background">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>Branch</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Action</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {categories.map((category) => (
+            <TableRow key={category.id}>
+              <TableCell className="font-medium">
+                {category.name}
+              </TableCell>
+
+              <TableCell>
+                {category.description}
+              </TableCell>
+
+              <TableCell>
+                {category.branch_id}
+              </TableCell>
+
+              <TableCell>
+                {category.is_active
+                  ? "Active"
+                  : "Inactive"}
+              </TableCell>
+
+              <TableCell>
+                <Link
+                  href={`/category/${category.id}`}
+                  className="text-primary hover:underline"
+                >
+                  View
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
